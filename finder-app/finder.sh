3cb85d8f69f4f0d94385dfebd56ff63023069f8e
sana-all <magigingbestlawyerako21@gmail.com>
2025-10-30 04:30:53 +0800

    commit 6

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,16 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ $# -ne 2 ]; then
-      echo "Invalid Number of arguments, please specify directory and text"
-      exit 1
-else
-	filesdir=$1
-	searchstr=$2
-	if [ ! -d "$filesdir" ]; then
-		echo "Directory not found, please check"
-		exit 1
-	else
-		number_of_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
-		number_of_files=$(grep -r -l "$searchstr" "$filesdir" | wc -l)
-		echo "The number of files are $number_of_files and the number of matching lines are $number_of_lines"
-	fi
+    echo "Error: Both parameters (directory and search string) must be specified."
+    exit 1
 fi
+
+filesdir="$1"
+searchstr="$2"
+
+if [ ! -d "$filesdir" ]; then
+    echo "Error: '$filesdir' is not a valid directory."
+    exit 1
+fi
+
+file_count=$(find "$filesdir" -type f | wc -l)
+matching_lines=$(grep -r -c "$searchstr" "$filesdir" | awk -F: '{sum += $2} END {print sum}')
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to search for '$searchstr' in '$filesdir'."
+    exit 1
+fi
+
+echo "The number of files are $file_count and the number of matching lines are $matching_lines"
